e148b0dafc29566003de5f57d6ca58d8038cbf2c
BingBlingBoy <ckeiranbarnes@gmail.com>
2023-09-20 09:08:36 +0100

    Fixing frontend errors

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,18 +3,18 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-      watch: {
-          usePolling: true
-      },
-      proxy: {
-          '/api': {
-              target: 'https://myalbumlist-api.onrender.com',
-              changeOrigin: true,
-              rewrite: (path) => path.replace(/^\/api/, ''),
-          },
-      }
-  }
+    plugins: [react()],
+    server: {
+        watch: {
+            usePolling: true
+        },
+        proxy: {
+            '/api': {
+                target: 'https://myalbumlist-api.onrender.com',
+                changeOrigin: true,
+                secure: false,
+            },
+        }
+    }
 })
 
