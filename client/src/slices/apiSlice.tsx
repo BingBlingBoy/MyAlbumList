29383f17a203739570cb90d7fa810ed6412d631a
BingBlingBoy <ckeiranbarnes@gmail.com>
2023-11-06 13:40:55 +0000

    Fixing errors

--- client/src/slices/apiSlice.tsx
+++ client/src/slices/apiSlice.tsx
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://myalbumlist-api.onrender.com/' })
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://myalbumlist-api.onrender.com' })
 
 export const apiSlice = createApi({
     baseQuery,
